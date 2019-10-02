88aceb5213812063a7aa1f4c10f53ddf8858447b
craigpatrickhunter <craig.p.hunter@gmail.com>
2019-10-02 09:54:46 -0700

    Updates

---
+++ precache-manifest.2ccda833730ee5844c27bdd6a20c9795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd00dfddd9284c315adb1eedfdc94ea6",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3080a1d62a7ccbc17153",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "11db071cde1d7872de31",
+    "url": "/my-app/static/js/2.9b0a676c.chunk.js"
+  },
+  {
+    "revision": "3080a1d62a7ccbc17153",
+    "url": "/my-app/static/js/main.bf5e566c.chunk.js"
+  },
+  {
+    "revision": "6a2b462ae9dc00bf631e",
+    "url": "/my-app/static/js/runtime-main.813740b8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
